c5912bf4477e8a25a9a93996303ac11beb4a2296
expit <j.olizarowicz@gmail.com>
2021-01-22 22:40:39 +0100

    Added cobra CLI

--- main.go
+++ main.go
@@ -1,45 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import "termgames/fastReader"
+import "github.com/expit/termgames/cmd"
 
 func main() {
-	fastReader.Gamestart()
-	//game := tl.NewGame()
-	//
-	//level := tl.NewBaseLevel(tl.Cell{
-	//	Bg: tl.ColorDefault,
-	//	Fg: tl.ColorDefault,
-	//	Ch: ' ',
-	//})
-	//
-	////q1num := fastReader.NumberGenerator(float64(2))
-	////q1str := strconv.Itoa(q1num)
-	////q1answered := true
-	//dashboard := Dashboard{
-	//	questions: []Question{
-	//		{
-	//			question: tl.NewText(10, 10, strconv.Itoa(fastReader.NumberGenerator(float64(5))), tl.ColorGreen, tl.ColorBlack),
-	//			visible: false,
-	//			answered: false,
-	//		},
-	//		{
-	//			question: tl.NewText(11, 11, strconv.Itoa(fastReader.NumberGenerator(float64(5))), tl.ColorGreen, tl.ColorBlack),
-	//			visible:  false,
-	//			answered: false,
-	//		},
-	//	},
-	//	answer:           tl.NewText(8, 6, "answer", tl.ColorGreen|tl.AttrBold, tl.ColorBlack),
-	//	rectTangle: tl.NewRectangle(1,1,1,1,tl.ColorBlack),
-	//	posX:             2,
-	//	posY:             2,
-	//	questionsVisible: true,
-	//	framesVisible:    50,
-	//	tick:             0,
-	//	questionMaxLengts: 0,
-	//}
-	//level.AddEntity(&dashboard)
-	//
-	//game.Screen().SetLevel(level)
-	//game.Screen().SetFps(64)
-	//game.Start()
+	cmd.Execute()
 }
